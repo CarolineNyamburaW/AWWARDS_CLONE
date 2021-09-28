f469e3e7ac3dd682e812aec01241948835632e9f
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-09-28 13:13:08 +0300

    made changes

--- awwwardz/settings.py
+++ awwwardz/settings.py
@@ -155,6 +155,7 @@ cloudinary.config(
   cloud_name = config('cloud_name'), 
   api_key = config('api_key'), 
   api_secret = config('api_secret'),
+  CLOUDINARY_URL = config('CLOUDINARY_URL')
 )
 
 LOGIN_REDIRECT_URL = "/"
